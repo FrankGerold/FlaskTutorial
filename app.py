65a6c8f24448989bf66a62c43257663042133658
Frank Gerold <FrankETSGerold@gmail.com>
2020-03-14 19:40:23 -0400

    Created DB

--- app.py
+++ app.py
@@ -1,10 +1,61 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
+import sqlite3
 
+# Configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'ayyy_lmao'
+USERNAME = '123'
+PASSWORD = '123'
+
+# Create and Initialize app
 app = Flask(__name__)
+app.config.from_object(__name__)
+#
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Connect to database
+def connect_db():
+    """
+    Connects to database.
+    """
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+# Create DB
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# Open db connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+# Close db cnxn
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
+    init_db()
     app.run()
--- apptest.py
+++ apptest.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from app import app
 
@@ -7,8 +8,13 @@ class BasicTestCase(unittest.TestCase):
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!')
+        # self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.status_code, 404)
+
+    def test_database(self):
+        tester = os.path.exists("flaskr.db")
+        self.assertTrue(tester)
 
 if __name__ == '__main__':
     unittest.main()
